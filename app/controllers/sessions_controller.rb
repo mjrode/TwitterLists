5aadb3d6c13a282ec9f71ed94129bccda8b77b67
mjrode <michaelrode44@gmail.com>
2016-05-20 18:36:47 -0400

    Set current_user with sessions to use in scoping methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
   def create
     response = Users::UserAuthenticationFromOauth.run(auth_hash: request.env["omniauth.auth"])
-    redirect_to root_path
+    @user = response.user
+    session[:user_id] = @user.id
+    flash[:success] = response.message
+    redirect_to pages_home_path
   end
 
   def destroy
