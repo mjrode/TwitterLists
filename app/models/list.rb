2c008c1c9602e74f046da3725de51e844bcbe869
mjrode <michaelrode44@gmail.com>
2016-05-20 18:33:38 -0400

    Added validations for models to prevent duplicate caching.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,9 @@
 class List < ActiveRecord::Base
   has_many :friend_list_schedules, dependent: :destroy
   has_many :friends, through: :friend_list_schedules
+  validates :api_list_id, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, uniqueness: true
   has_many :friends, dependent: :destroy
   has_many :lists, dependent: :destroy
 end
