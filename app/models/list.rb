0ef8a4958fbdabd27bdd80eee32779fb18d6e234
mjrode <michaelrode44@gmail.com>
2016-05-23 09:55:06 -0400

    Fix associations for users/lists.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
   has_many :friend_list_schedules, dependent: :destroy
   has_many :friends, through: :friend_list_schedules
-  validates :api_list_id, uniqueness: true
+  belongs_to :user
   validates :name, presence: true
   validates :name, uniqueness: true
 
