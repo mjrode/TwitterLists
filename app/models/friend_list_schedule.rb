5cae2cf97b71a3714a3556c50a4ae2ea48ec6360
mjrode <michaelrode44@gmail.com>
2016-05-23 15:43:32 -0400

    Set constants for schedule options.

--- app/models/friend_list_schedule.rb
+++ app/models/friend_list_schedule.rb
@@ -1,4 +1,9 @@
 class FriendListSchedule < ActiveRecord::Base
   belongs_to :list
   belongs_to :friend
+
+  def self.schedules
+    ["Never on List", "Always on List", "Rotates on and off list"]
+  end
+
 end
