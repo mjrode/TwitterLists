3a195a297ea5372fafee644f26dd20b6286b0651
mjrode <michaelrode44@gmail.com>
2016-05-24 13:16:31 -0400

    Specified when talking about remote vs local for clarity.

--- app/interactions/users/import_lists.rb
+++ app/interactions/users/import_lists.rb
@@ -2,42 +2,49 @@ class Users::ImportLists < Less::Interaction
   expects :username
 
   def run
-    cache_lists
+    fetch_remote_lists
   end
 
   private
 
-  def get_list_ids_from_twitter
+  def remote_lists
     TWITTER_CLIENT.lists(username)
   end
 
-  def cache_lists
-    get_list_ids_from_twitter.each do |list|
-      save_list_id(list)
-      # commenting out due to rate limit issues
-      # create_schedule(list)
+  def fetch_remote_lists
+    remote_lists.each do |remote_list|
+      save_local_list(remote_list)
+      create_schedule(remote_list)
     end
   end
 
 
-  def save_list_id(list)
+  def save_local_list(remote_list)
     user = User.find_by_username(username)
     List.create(
-      name: list.name,
-      remote_id:  list.id, 
+      name: remote_list.name,
+      remote_id:  remote_list.id, 
       user_id:  user.id)
   end
 
-  def create_schedule(list)
+  def remote_members(remote_list)
+    TWITTER_CLIENT.list_members(username, remote_list.id)    
+  end
+
+  def local_list(remote_list)
+    List.find_by_remote_id(remote_list.id)
+  end
+
+  def local_friend(remote_member)
+      friend = Friend.find_by_remote_id(remote_member.id)
+  end
+
+  def create_schedule(remote_list)
     # rate limit issues here
-    binding.pry
-    members = TWITTER_CLIENT.list_members(username, list.id)
-    list = List.find_by_remote_id(list.id)
-    members.each do |member|
-      friend = Friend.find_by_remote_id(member.id)
+    remote_members(remote_list).each do |remote_member|
       FriendListSchedule.create(
-        list_id: list.id, 
-        friend_id: friend.id, 
+        list_id: local_list(remote_list).id, 
+        friend_id: local_friend(remote_member).id, 
         schedule: "Always on List" )
     end
   end
