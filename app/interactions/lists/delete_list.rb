0a44e589494caed1abba79d4200998515d0d58d5
mjrode <michaelrode44@gmail.com>
2016-05-24 15:20:14 -0400

    Created interaction to delete remote list when local list is deleted.

---
+++ app/interactions/lists/delete_list.rb
@@ -0,0 +1,29 @@
+class Lists::DeleteList < Less::Interaction
+  expects :list
+  expects :user
+
+  def run
+  set_twitter_client  
+  destroy_remote_list
+  end
+
+
+  private
+
+  def set_twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["twitter_consumer_key"]
+      config.consumer_secret     = ENV["twitter_secret_key"]
+      config.access_token        = user.token
+      config.access_token_secret = user.secret
+    end
+  end
+
+  def destroy_remote_list
+    binding.pry
+    @client.destroy_list(list.remote_id)
+  end
+
+
+
+end
