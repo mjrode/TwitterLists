5d7447ae440802b9f373c4f298947e681962bc35
mjrode <michaelrode44@gmail.com>
2016-05-30 13:39:02 -0400

    Added tests for user_authentication interaction.

--- test/interactions/user/list_of_friends_test.rb
+++ test/interactions/user/list_of_friends_test.rb
@@ -1,8 +1,3 @@
 require 'test_helper'
 
-class User::ListOfFriendsTest < ActiveSupport::TestCase
-  test 'it gets called' do
-    User::ListOfFriends.run
-    pass
-  end
-end
+
--- test/interactions/users/list_of_friends_test.rb
+++ test/interactions/users/list_of_friends_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
-class Users::ListOfFriendsTest < ActiveSupport::TestCase
-  test 'it gets called' do
-    Users::ListOfFriends.run
-    pass
-  end
-end
+
+
