fef490f156a5ad8707019abd4e6821e774356172
mjrode <michaelrode44@gmail.com>
2016-06-04 11:45:44 -0400

    Added tests for import_users_friends.rb interaction.

--- test/interactions/users/import_users_friends_test.rb
+++ test/interactions/users/import_users_friends_test.rb
@@ -3,18 +3,31 @@ require 'test_helper'
 class Users::ImportUsersFriendsTest < ActiveSupport::TestCase
   def setup
     @user = User.new(
-      username: "mjr_tts",  
-      token: '711995065347006465-JLJWrBLyfySojWUz1C7mwaqx37Qv7vQ', 
+      username: "mjr_tts",
+      token: '711995065347006465-JLJWrBLyfySojWUz1C7mwaqx37Qv7vQ',
       secret: 'Efmd45AlTJml6nIcsaBVjLuyZwPpx9Bn9ktICwiPxNIRF'
     )
   end
 
   test "adds friends to database" do
-    use_cassette("importthemfriends") do 
+    use_cassette("import_friends") do
       assert_changed -> { Friend.count } do
         Users::ImportUsersFriends.run(user: @user)
-        puts Friend.count
       end
     end
+  end
+
+  test "adds friends that are only on a list to database" do 
+    use_cassette("import_friends") do 
+      Users::ImportUsersFriends.run(user: @user)
+    end
+    assert Friend.find_by_username("Coalboat").present?
+  end
+
+  test "followers that are not your friends do not get added" do 
+    use_cassette("import_friends") do 
+      Users::ImportUsersFriends.run(user: @user)
+    end
+    assert Friend.find_by_username("mjrode").blank?
   end
 end
