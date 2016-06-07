9d178198c9bfd448119c6fb80f61b8fbfd69cf1e
mjrode <michaelrode44@gmail.com>
2016-06-07 10:28:13 -0400

    Added test for update_remote_lists.rb interaction.

--- app/interactions/lists/update_remote_list_members.rb
+++ app/interactions/lists/update_remote_list_members.rb
@@ -4,15 +4,22 @@ class Lists::UpdateRemoteListMembers < Less::Interaction
   expects :user_id
 
   def run
-    set_user
-    set_twitter_client
     update_friends
+  end
+
+  def twitter_client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["twitter_consumer_key"]
+      config.consumer_secret     = ENV["twitter_secret_key"]
+      config.access_token        = user.token
+      config.access_token_secret = user.secret
+    end
   end
 
   private
 
-  def set_user
-    @user = User.find(user_id)
+  def user
+    @user ||= User.find(user_id)
   end
 
   def update_friends
@@ -22,8 +29,8 @@ class Lists::UpdateRemoteListMembers < Less::Interaction
 
   def current_list_of_friends
     current_list_of_friends = []
-    unless list.new_list?(@user)
-      remote_members = @client.list_members(list.remote_id)
+    unless list.new_list?(user)
+      remote_members = twitter_client.list_members(list.remote_id)
       remote_members.each do |member|
         current_list_of_friends << member.screen_name
       end
@@ -33,12 +40,12 @@ class Lists::UpdateRemoteListMembers < Less::Interaction
 
   def remove_members
     friends_to_remove = current_list_of_friends - list_of_randomized_friends
-    @client.remove_list_members(@user.remote_id, list.remote_id, friends_to_remove)
+    twitter_client.remove_list_members(user.remote_id, list.remote_id, friends_to_remove)
   end
 
   def add_members
     friends_to_add = list_of_randomized_friends - current_list_of_friends
-    @client.add_list_members(list.remote_id, friends_to_add)
+    twitter_client.add_list_members(list.remote_id, friends_to_add)
   end
 
   def list_of_randomized_friends
@@ -47,14 +54,5 @@ class Lists::UpdateRemoteListMembers < Less::Interaction
       friends << schedule.friend.username
     end
     friends
-  end
-
-  def set_twitter_client
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["twitter_consumer_key"]
-      config.consumer_secret     = ENV["twitter_secret_key"]
-      config.access_token        = @user.token
-      config.access_token_secret = @user.secret
-    end
   end
 end
--- test/interactions/lists/update_remote_list_members_test.rb
+++ test/interactions/lists/update_remote_list_members_test.rb
@@ -1,5 +1,5 @@
 # how to test without actually changing the members on the remote list
-# think that I need to mock the @client? 
+# think that I need to mock the @client?
 require 'test_helper'
 class Lists::UpdateRemoteListMembersTest < ActiveSupport::TestCase
   def setup
@@ -9,34 +9,36 @@ class Lists::UpdateRemoteListMembersTest < ActiveSupport::TestCase
       secret: 'Efmd45AlTJml6nIcsaBVjLuyZwPpx9Bn9ktICwiPxNIRF',
       remote_id: "711995065347006465"
     )
-    set_twitter_client
+    @friends_to_add = ["kendricklamar"]
+    @friends_to_remove = ["jasoncummings86"]
   end
 
   test "old list members get removed" do
-    use_cassette("update remote lists") do
-      Lists::UpdateRemoteListMembers.run(
-        randomized_list_of_friends: randomized_list_of_friends,
+    VCR.use_cassette("update remote lists") do
+      interaction = Lists::UpdateRemoteListMembers.new(
+        randomized_list_of_friends: list_of_friends,
         user_id: @user.id,
         list: lists(:first_list)
       )
-      members = @client.list_members(lists(:first_list).remote_id)
+      interaction.twitter_client.expects(:remove_list_members).with(
+        @user.remote_id,
+        lists(:first_list).remote_id,
+        @friends_to_remove
+      )
+      interaction.twitter_client.expects(:add_list_members).with(
+        lists(:first_list).remote_id,
+        @friends_to_add
+      )
+      
+      interaction.run
     end
   end
+
 
   private
 
-  def randomized_list_of_friends
-    @fls = []
-    @fls.push(friend_list_schedules(:two), friend_list_schedules(:three))
-    @fls
-  end
-
-  def set_twitter_client
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["twitter_consumer_key"]
-      config.consumer_secret     = ENV["twitter_secret_key"]
-      config.access_token        = @user.token
-      config.access_token_secret = @user.secret
-    end
+  def list_of_friends
+    # one = kendrick, two = sports, three = jason
+    [friend_list_schedules(:two), friend_list_schedules(:one)]
   end
 end
