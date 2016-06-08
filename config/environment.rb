d08f49ec28f34f02667605907a39a2bf14461937
mjrode <michaelrode44@gmail.com>
2016-06-08 09:44:08 -0400

    Set mailer configuration and file to store env variables.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com', 
+  port: 587, 
+  domain: 'http://localhost:3000/', 
+  user_name: ENV[:email], 
+  password: ENV[:password]
+}
