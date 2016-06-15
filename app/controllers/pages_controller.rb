ff1de530bdc9c59ba79d256f6fd4b10537f6271f
mjrode <michaelrode44@gmail.com>
2016-06-15 12:33:41 -0400

    Styled log in page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @email_text = "Add Email Address" if current_user.email.nil?
-    @email_text = "Edit Email Address" if current_user.email.present?
+    if current_user
+      @email_text = "Add Email Address" if current_user.email.nil?
+      @email_text = "Edit Email Address" if current_user.email.present?
+    end
   end
 end
