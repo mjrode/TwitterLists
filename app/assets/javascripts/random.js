9748c33b56929f76c8c4763b4348210b22254597
mjrode <michaelrode44@gmail.com>
2016-06-17 11:07:24 -0400

    Added google analytics script

---
+++ app/assets/javascripts/random.js
@@ -0,0 +1,11 @@
+// $("document").ready(function() {
+//   $("a").css("color", "red");
+//   // $("#friends__list_name").css("color", "red");
+
+//   $('#friends__list_name').change(function () {
+//     $("#friends__list_name").append('<option value = "option6"> option6 </option> ')
+//     $('#friends__list_name').clone().appendTo(".clear");
+
+//   })
+
+// })
