a2c86bbe5df3bfe29050dc8a2188e79a333573b6
mjrode <michaelrode44@gmail.com>
2016-06-20 16:38:47 -0400

    Added Jquery to change background based on selection

--- app/assets/javascripts/random.js
+++ app/assets/javascripts/random.js
@@ -9,6 +9,18 @@ $("document").ready(function() {
   });
   
 
+  $('.shrink-me').each(function (index, value) { 
+    $("#friend-list-options-"+index).change(function(){
+      var colors = {1: 'rgba(53, 214, 37, 0.639216)', 2: '#fee873', 3: 'rgba(232, 68, 68, 0.529412)', 4: 'white' };
+      var value = $(this).val()      
+      $(".color-me-"+index).css('background', colors[value]);
+    });
 
-})
+      var colors = {1: 'rgba(53, 214, 37, 0.639216)', 2: '#fee873', 3: 'rgba(232, 68, 68, 0.529412)', 4: 'white' };
+      var value = $("#friend-list-options-"+index).val()      
+      $(".color-me-"+index).css('background', colors[value]);
+  });
+
+
+});
 
