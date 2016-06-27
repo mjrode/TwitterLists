58d98af7649014f1f1bb91ee1732ed3ec16e87cd
mjrode <michaelrode44@gmail.com>
2016-06-27 16:07:47 -0400

    Added animation to home page

--- app/assets/javascripts/random.js
+++ app/assets/javascripts/random.js
@@ -6,30 +6,31 @@ $("document").ready(function() {
       $("#select-list").animate({
          height: 'toggle'
       });
+      $('.goaway').toggle();
   });
   
 
   $('.shrink-me').each(function (index, value) { 
     $("#friend-list-options-"+index).change(function(){
       var colors = {1: 'rgba(53, 214, 37, 0.639216)', 2: '#fee873', 3: 'rgba(232, 68, 68, 0.529412)', 4: 'white' };
-      var div = {1: '#always-on', 2: '#frequently-on', 3: '#sometimes-on'}
+      var div = {1: '#always-on', 2: '#frequently-on', 3: '#sometimes-on'};
       var value = $(this).val();      
       $(".color-me-"+index).css('background', colors[value]);
       $("#move-me-"+index).appendTo(div[value]);
       if(value!=4){
-        $("#move-hr-"+index).remove()
-      };
+        $("#move-hr-"+index).remove();
+      }
     });
 
-      var div = {1: '#always-on', 2: '#frequently-on', 3: '#sometimes-on'}
+      var div = {1: '#always-on', 2: '#frequently-on', 3: '#sometimes-on'};
       var colors = {1: 'rgba(53, 214, 37, 0.639216)', 2: '#fee873', 3: 'rgba(232, 68, 68, 0.529412)', 4: 'white' };
-      var value = $("#friend-list-options-"+index).val()      
+      var value = $("#friend-list-options-"+index).val();      
       $(".color-me-"+index).css('background', colors[value]);
       $(".color-me-"+index).css('background', colors[value]);
-      $("#move-me-"+index).appendTo(div[value])
+      $("#move-me-"+index).appendTo(div[value]);
       if(value!=4){
-        $("#move-hr-"+index).remove()
-      };
+        $("#move-hr-"+index).remove();
+      }
 
   });
 
