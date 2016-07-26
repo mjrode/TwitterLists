5cc65c37e6f28073295c7d51f6cdab15552d6a93
Michael Rode <Michaelrode44@gmail.com>
2016-07-26 17:25:24 -0400

    Removed old interaction

--- app/interactions/lists/randomize_and_update.rb
+++ app/interactions/lists/randomize_and_update.rb
@@ -1,32 +1,32 @@
-class Lists::RandomizeAndUpdate < Less::Interaction
-  expects :user
+# class Lists::RandomizeAndUpdate < Less::Interaction
+#   expects :user
 
-  def run
-    randomize_and_update_lists
-  end
+#   def run
+#     randomize_and_update_lists
+#   end
 
-  private
+#   private
 
-  def lists_to_update
-    user.lists.map do |list|
-      unless list.next_rotation_at != nil
-        puts list.id  if list.next_rotation_at < Time.now
-      end
-    end
-  end
+#   def lists_to_update
+#     user.lists.map do |list|
+#       unless list.next_rotation_at != nil
+#         puts list.id  if list.next_rotation_at < Time.now
+#       end
+#     end
+#   end
 
-  def randomize_and_update_lists
-    # lists_to_update.each do |list_id|
-    #   randomized_list = Lists::RandomizeList.run(list: List.find(list_id)
+#   def randomize_and_update_lists
+#     # lists_to_update.each do |list_id|
+#     #   randomized_list = Lists::RandomizeList.run(list: List.find(list_id)
         
-    #   Lists::UpdateRemoteListMembers.run(
-    #         randomized_list_of_friends: randomized_list,
-    #         list: List.find(list_id),
-    #         user_id: user.id
-    #       )
-    end  
-  end
+#     #   Lists::UpdateRemoteListMembers.run(
+#     #         randomized_list_of_friends: randomized_list,
+#     #         list: List.find(list_id),
+#     #         user_id: user.id
+#     #       )
+#     end  
+#   end
 
 
 
-end
+# end
